e84583776c753bf4ad8a096497fa288f5920f48e
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2023-04-20 17:46:01 -0400

    some more keys
    bump package version

--- extension.js
+++ extension.js
@@ -2,6 +2,13 @@ const vscode = require("vscode");
 
 const keys = {
   version: "[Data version](https://minecraft.fandom.com/wiki/Data_version) of the client version this file was last saved in; used for upgrading default settings. (numeric)",
+  autoJump: "Whether auto-jump is enabled",
+  autoSuggestions: "True if brigadier's command suggestion UI should always be shown, instead of just when pressing tab",
+  chatColors: "Whether colored chat is allowed",
+  chatLinks: "Whether links show as links or just text in the chat",
+  chatLinksPrompt: "Whether clicking on links in chat needs confirmation before opening them",
+  enableVsync: "Whether [v-sync](https://en.wikipedia.org/wiki/Screen_tearing#V-sync) (vertical synchronization) is enabled",
+  entityShadows: "Whether to display entity shadows",
 };
 
 vscode.languages.registerHoverProvider("options-txt", {
