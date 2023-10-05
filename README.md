379386d87544359cbed040de538a8bdf3e5db603
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2023-10-05 11:59:51 -0400

    add options intellisense up to maxFps
    bump package version [rev]
    
    behind the scenes update to README
    
    add Bedrock Edition options.txt with sensitive info removed (I think)

--- README.md
+++ README.md
@@ -19,10 +19,10 @@ This feature will automatically trigger for files with the names `options.txt` a
 ### Intellisense (sort of)
 
 The extension has been loaded with information about the various options and what they do.
-Hovering over text in a line will provide a hover note with a short blurb about what that option controls (taken from the [Minecraft Wiki page][fandom/options.txt/je]).
+Hovering over text in a line will provide a hover note with a short blurb about what that option controls (taken from the [Minecraft Wiki page][wiki/options.txt/je]).
 
 ![Hover Information](./screenshots/options.txt.hover-info.png)
 
 This feature is not complete in this release, but version `1.0.0` will have information concerning all of the vanilla options.
 
-[fandom/options.txt/je]: https://minecraft.wiki/w/Options.txt#Java_Edition
+[wiki/options.txt/je]: https://minecraft.wiki/w/Options.txt#Java_Edition
--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 const vscode = require("vscode");
 
 const keys = {
-  version: "[Data version](https://minecraft.fandom.com/wiki/Data_version) of the client version this file was last saved in; used for upgrading default settings. (numeric)",
+  version: "[Data version](https://minecraft.wiki/w/Data_version) of the client version this file was last saved in; used for upgrading default settings. (numeric)",
   autoJump: "Whether auto-jump is enabled",
   autoSuggestions: "True if brigadier's command suggestion UI should always be shown, instead of just when pressing tab",
   chatColors: "Whether colored chat is allowed",
@@ -9,6 +9,32 @@ const keys = {
   chatLinksPrompt: "Whether clicking on links in chat needs confirmation before opening them",
   enableVsync: "Whether [v-sync](https://en.wikipedia.org/wiki/Screen_tearing#V-sync) (vertical synchronization) is enabled",
   entityShadows: "Whether to display entity shadows",
+  forceUnicodeFont: "Whether Unicode font should be used",
+  discrete_mouse_scroll: "Ignores scrolling set by operating system",
+  invertYMouse: "Whether mouse is inverted or not",
+  realmsNotifications: "Whether Realms invites are alerted on the main menu",
+  reducedDebugInfo: "Whether to show reduced information on the [Debug screen](https://minecraft.wiki/w/Debug_screen)",
+  showSubtitles: "If [subtitles](https://minecraft.wiki/w/Subtitles) are shown",
+  directionalAudio: "	If [Directional Audio](https://en.wikipedia.org/wiki/Directional_sound) is enabled",
+  touchscreen: "Whether touchscreen controls are used",
+  fullscreen: "Whether the game attempts to go fullscreen at startup",
+  bobView: "Whether or not the camera bobs up and down as the player walks",
+  toggleCrouch: "Whether the sneak key must be pressed or held to activate sneaking",
+  toggleSprint: "Whether the sprint key must be pressed or held to activate sprinting",
+  darkMojangStudiosBackground: "Whether the Mojang Studios loading screen will appear monochrome",
+  hideLightningFlashes: "	Hide lightning flashes (visual effect)",
+  mouseSensitivity: "How much a mouse movement changes the position of the camera",
+  fov: "How large the field of view is (floating-point). The in-game value is counted in degrees, however, the *options.txt* isn't. The value is converted into degrees with the following formula: `degrees = 40 * value + 70`",
+  screenEffectScale: "Distortion Effects (how intense the effects of [Nausea](https://minecraft.wiki/w/Nausea) and [nether portals](https://minecraft.wiki/w/Nether_portal) are)",
+  fovEffectScale: "FOV Effects (how much the field of view changes when sprinting, having [Speed](https://minecraft.wiki/w/Speed) or [Slowness](https://minecraft.wiki/w/Slowness) etc.)",
+  darknessEffectScale: "Darkness Pulsing (how much the [Darkness](https://minecraft.wiki/w/Darkness) effect pulses)",
+  gamma: "Brightness",
+  renderDistance: "The render distance in the chunk radius from the player",
+  simulationDistance: "The simulation distance in the chunk radius from the player",
+  entityDistanceScaling: "The maximum distance from the player that entities render",
+  guiScale: "Size of interfaces",
+  particles: "Amount of particles (such as rain, potion effects, etc.)",
+  maxFps: "The maximum framerate",
 };
 
 vscode.languages.registerHoverProvider("options-txt", {
